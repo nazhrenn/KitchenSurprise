c83f8583de74ecda9383c1384f950ca984fdde2b
Tyler Coles <tylerevancoles@gmail.com>
2015-11-21 19:29:21 -0500

    Added bower support, basic jade template and angular framework.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-﻿var express = require('express');
-var app = express();
+﻿var express = require('express'),
+	jade = require('jade'),
+	app = express();
+
+// setup express.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use('/static', express.static('static'));
+app.use('/vendor', express.static('vendor'));
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.render('main', { title: "Kitchen Surprise" });
 });
 
 var server = app.listen(3000, function () {
---
+++ static/app/kitchen.js
@@ -0,0 +1,21 @@
+﻿var kitchenApp = angular.module('kitchenApp', ['ngRoute']);
+
+kitchenApp.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.
+	when('/home', {
+			templateUrl: 'static/views/home.html',
+			controller: 'homeController',
+			controllerAs: 'home'
+		})
+	.otherwise({
+			redirectTo: '/home'
+		});
+}]);
+
+kitchenApp.controller("masterController", function () {
+
+});
+
+kitchenApp.controller("homeController", function () {
+
+});
